c13f372f4e0a912c3ac310656646d3d7958656c1
franxx1 <fransiskusandre4@gmail.com>
2025-12-03 23:18:53 +0800

    feat: Add DefaultLayout component

---
+++ src/components/DefaultLayout.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Navbar from './navbar.vue';
+import Footer from './footer.vue';
+</script>
+<template>
+  <Navbar />
+  <div class="pt-[100px]">
+    <slot />
+  </div>
+</template>
