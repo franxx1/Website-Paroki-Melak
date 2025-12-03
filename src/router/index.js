5b77601f0a00398eee68cab45b6270bd0804c00c
franxx1 <fransiskusandre4@gmail.com>
2025-12-03 23:18:53 +0800

    feat: Add routes for galeri and paroki pages

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,14 @@ const PengumumanDetail = () => import("../components/pages/berita-dan-pengumuman
 
 const Sakramen = () => import("../components/pages/sakramen/sakramen.vue");
 const SakramenDetail = () => import("../components/pages/sakramen/sakramen_detail.vue");
+const Sejarah = () => import("../components/pages/paroki/sejarah.vue");
+const Dpp = () => import("../components/pages/paroki/dpp.vue");
+const Pastor = () => import("../components/pages/paroki/pastor.vue");
+const Jadwal_Misa = () => import("../components/jadwalMisa.vue");
+const Pelayanan = () => import("../components/pelayanan.vue")
+const Galeri = () => import("../components/pages/galeri/galeri.vue")
+const GaleriDetail = () => import("../components/pages/galeri/galeri_detail.vue")
+
 
 const routes = [
   {
@@ -47,6 +55,41 @@ const routes = [
     path: "/sakramen/:uid",
     name: "sakramenDetail",
     component: SakramenDetail,
+  },
+  {
+    path: "/sejarah",
+    name: "sejarah",
+    component: Sejarah,
+  },
+  {
+    path: "/dpp",
+    name: "dpp",
+    component: Dpp,
+  },
+  {
+    path: "/pastor",
+    name: "pastor",
+    component: Pastor,
+  },
+  {
+    path: "/jadwal-misa",
+    name: "jadwalMisa",
+    component: Jadwal_Misa,
+  },
+  {
+    path: "/pelayanan",
+    name: "pelayanan",
+    component: Pelayanan,
+  },
+  {
+    path: "/galeri",
+    name: "galeri",
+    component: Galeri,
+  },
+  {
+    path: "/galeri/:uid",
+    name: "galeriDetail",
+    component: GaleriDetail,
   },
 ];
 
